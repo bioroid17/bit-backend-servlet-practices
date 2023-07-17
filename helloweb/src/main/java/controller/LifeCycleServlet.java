65986e0037519b5688a85bf13cdf90edc18cc7f9
bioroid17 <trace1997@ajou.ac.kr>
2023-07-17 14:43:49 +0900

    서블릿의 생명주기

--- helloweb/src/main/java/controller/LifeCycleServlet.java
+++ helloweb/src/main/java/controller/LifeCycleServlet.java
@@ -6,9 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LifeCycleServlet
- */
+
 public class LifeCycleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
